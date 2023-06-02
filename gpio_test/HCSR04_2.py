bcd454064c1e3f6d894803edaada118f08118786
Carre Chu <mariokart761@gmail.com>
2023-06-02 16:21:18 -0700

    Fix annotation

--- gpio_test/HCSR04_2.py
+++ gpio_test/HCSR04_2.py
@@ -10,24 +10,23 @@ time.sleep(1)
 
 def get_distance():
     GPIO.output(Trig_Pin, GPIO.HIGH)
-    # 将Trig脚位拉高电位输出超声波信号，等待100微秒之后关闭输出
     time.sleep(0.000100) # wait at least 10 microsecond
     GPIO.output(Trig_Pin, GPIO.LOW)
     
-    # 在Echo收到信号前后分别记录时间
-    timeout = time.time() + 0.5  # 设置超时时间为0.5秒钟
+    # Echo收到信號前分別記錄時間
+    timeout = time.time() + 0.5  # 設定timeout時間為0.5秒
     while not GPIO.input(Echo_Pin):
         if time.time() > timeout:
-            return None  # 如果超时则返回None
+            return None  # 超時則return None
     t1 = time.time()
     
-    timeout = time.time() + 0.5  # 设置超时时间为0.5秒钟
+    timeout = time.time() + 0.5  # 設定timeout時間為0.5秒
     while GPIO.input(Echo_Pin):
         if time.time() > timeout:
-            return None  # 如果超时则返回None
+            return None  # 超時則return None
     t2 = time.time()
     
-    # 最后套用速度公式算出距离返回
+    # return 套速度公式計算出的距離
     return (t2-t1)*340*100/2
 
 if __name__ == "__main__":
